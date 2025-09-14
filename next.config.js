b2fa5ec872bc65a90769721b8b8245a49ec55ed9
ruifengzhaomail <ruifengzhaomail@gmail.com>
2025-09-14 20:38:49 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,24 @@
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'localhost'
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'http', 
+        hostname: 'localhost',
+        port: '3001',
+      }
+    ],
+    formats: ['image/webp', 'image/avif'],
+  },
+}
+
+module.exports = nextConfig

8cbf3f4d9077b56b29d5c053f65f84961e277f68
ruifengzhaomail <ruifengzhaomail@gmail.com>
2025-10-03 23:20:27 +0800

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,1 +1,12 @@
 
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://www.agromachineryexport.com/sitemap.xml',
+  }
+}
